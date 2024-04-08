dad496396323059fad2931cb596a155f839cecd5
YieGin <pumasami7@gmail.com>
2024-04-09 00:53:11 +0100

    Shorten Profile Section description text

--- src/components/Profile-Section.tsx
+++ src/components/Profile-Section.tsx
@@ -26,36 +26,20 @@ const ProfileSection = () => {
           <h1 className="sm:text-3xl font-bold md:hidden block">
             Islam Belamri
           </h1>
-          <p className="md:text-[0.9rem] text-[0.7rem] text-muted-foreground mt-2 md:mr-5">
-            Since beginning my coding journey in 2021, I have honed my skills as
-            a full stack developer through continuous learning and practical
-            application. My foundational expertise started with front-end
-            technologies—HTML, CSS, and JavaScript—quickly advancing to
-            React.js, where I mastered ES6 features, React Router, Hooks, Redux
-            Toolkit, Context API, and React Query. With a solid grasp of
-            front-end development, I expanded my proficiency into UI/UX
-            principles, adopting best practices that significantly enhanced the
-            user interfaces of my projects. My transition to full-stack
-            development was marked by embracing Next.js, leveraging its
-            capabilities to build scalable and dynamic web applications. As I
-            ventured into backend development, I began with Python and explored
-            web frameworks like Flask and Django. I gained competency in both
-            SQL and NoSQL databases, including MySQL, PostgreSQL, and MongoDB,
-            enriching my full-stack development toolkit. My first full-stack
-            endeavor—a book-selling platform—showcased my integrated skills,
-            employing Next.js 14, Tailwind CSS, Django, JWT authentication, and
-            HTTP-only cookies to bolster security. Continuing my growth, I
-            explored the MERN stack, delving into Node.js, Express.js, and
-            MongoDB. This journey was supplemented by three comprehensive
-            courses, each sharpening different facets of backend development. My
-            portfolio of full-stack projects includes a sophisticated food
-            ordering website, crafted using Next.js, Tailwind CSS, Node.js,
-            Express.js, and MongoDB, with Auth0 integration for secure user
-            authentication. Currently, I&apos;m dedicated to expanding my
-            project repertoire and embracing new challenges. As a fast learner
-            and a collaborative team player, I&apos;m always ready to explore
-            and adapt to new technologies, ensuring that my contributions are
-            impactful andd innovative.
+          <p className="md:text-[0.9rem] text-[0.7rem] text-muted-foreground mt-2 md:mr-5 leading-7">
+            Since 2021, I&apos;ve evolved from mastering front-end technologies like
+            HTML, CSS, and JavaScript to becoming a proficient full-stack
+            developer. My journey included deep dives into React.js, mastering
+            advanced features and state management, and expanding into UI/UX
+            design principles. Transitioning to full-stack with Next.js, I&apos;ve
+            built scalable applications, moving into backend development with
+            Python, Flask, Django, and the MERN stack. Highlights of my work
+            include a secure book-selling platform and a sophisticated food
+            ordering website, showcasing my skills in database management,
+            security, and user authentication. Through continuous learning and
+            practical application, including three in-depth backend courses, I&apos;m
+            committed to growth, eagerly tackling new technologies and complex
+            challenges in a collaborative environment.
           </p>
           <h3 className="absolute -right-44 xl:-right-48 top-1/2 rotate-90 tracking-wide text-3xl font-Pacifico md:block hidden">
             {letters}
