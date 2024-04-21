d1e736e32f0c04b7a6a6f5d50860c83c1208628f
YieGin <pumasami7@gmail.com>
2024-04-21 09:57:32 +0100

    .

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,6 +56,7 @@ const config = {
           DEFAULT: "hsl(var(--accent))",
           foreground: "hsl(var(--accent-foreground))",
         },
+        // Islam,
         popover: {
           DEFAULT: "hsl(var(--popover))",
           foreground: "hsl(var(--popover-foreground))",
