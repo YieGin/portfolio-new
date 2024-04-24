ada6f499c53c12c93791efdcf8454a4dadfb125d
YieGin <pumasami7@gmail.com>
2024-04-24 01:57:05 +0100

    changed Hero image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
         </motion.div>
         <div className="flex items-center justify-center mt-10 sm:w-max">
           <Avatar className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <AvatarImage className="object-cover" src="https://i.ibb.co/h9Vx4Lv/image.png" />
+            <AvatarImage className="object-cover" src="https://res.cloudinary.com/dnttuevkd/image/upload/v1713920022/oefhvxm9zbrmqfpqnuit.png" />
           </Avatar>
         </div>
       </div>
