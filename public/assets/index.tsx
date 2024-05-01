973e95a197d717d09e15e9024eb55e94566a4940
YieGin <pumasami7@gmail.com>
2024-05-01 10:52:52 +0100

    fixed import at public

--- public/assets/index.tsx
+++ public/assets/index.tsx
@@ -2,14 +2,12 @@
 export { default as Logo } from "./svgs/logo-color.svg";
 
 // LandingPage
-export { default as BiteZImg } from "./Images/BiteZImg.webp";
-export { default as BookoeImg } from "./Images/BookoeImg.webp";
-export { default as InpuctImg } from "./Images/InpuctImg.webp";
-export { default as PcImg } from "./Images/Pc.webp";
-export { default as PcImgTwo } from "./Images/Pc2.webp";
-export { default as Profile } from "./Images/Profile.webp";
-export { default as RolexImg } from "./Images/RolexImg.webp";
-export { default as VanguardEliteImg } from "./Images/VanguardEliteImg.webp";
+export { default as BiteZImg } from "./Images/bitezimg.webp";
+export { default as BookoeImg } from "./Images/bookoeimg.webp";
+export { default as InpuctImg } from "./Images/inpuctimg.webp";
+export { default as PcImgTwo } from "./Images/pc2.webp";
+export { default as Profile } from "./Images/profile.webp";
+export { default as VanguardEliteImg } from "./Images/vanguardeliteimg.webp";
 export { default as ProfileHero } from "./Images/profile-pic.webp";
 
 // About Page
@@ -21,20 +19,20 @@ export { default as P6 } from "./Images/p6.webp";
 export { default as P7 } from "./Images/p7.webp";
 
 // HardwareList
-export { default as Case } from "./Images/Case.webp";
-export { default as Chair } from "./Images/Chair.webp";
-export { default as Cooler } from "./Images/Cooler.webp";
-export { default as Cpu } from "./Images/Cpu.webp";
-export { default as Disk } from "./Images/Disk.webp";
-export { default as Graphics } from "./Images/Graphics.webp";
-export { default as Headset } from "./Images/Headset.webp";
-export { default as Keyboard } from "./Images/Keyboard.webp";
-export { default as Memory } from "./Images/Memory.webp";
-export { default as Microphone } from "./Images/Microphone.webp";
-export { default as Monitor } from "./Images/Monitor.webp";
-export { default as Motherboard } from "./Images/Motherboard.webp";
-export { default as Mouse } from "./Images/Mouse.webp";
-export { default as Power } from "./Images/Power.webp";
+export { default as Case } from "./Images/case.webp";
+export { default as Chair } from "./Images/chair.webp";
+export { default as Cooler } from "./Images/cooler.webp";
+export { default as Cpu } from "./Images/cpu.webp";
+export { default as Disk } from "./Images/disk.webp";
+export { default as Graphics } from "./Images/graphics.webp";
+export { default as Headset } from "./Images/headset.webp";
+export { default as Keyboard } from "./Images/keyboard.webp";
+export { default as Memory } from "./Images/memory.webp";
+export { default as Microphone } from "./Images/microphone.webp";
+export { default as Monitor } from "./Images/monitor.webp";
+export { default as Motherboard } from "./Images/motherboard.webp";
+export { default as Mouse } from "./Images/mouse.webp";
+export { default as Power } from "./Images/power.webp";
 
 // SoftwareList
 export { default as anydo } from "./Images/anydo.webp";
@@ -51,7 +49,7 @@ export { default as visual } from "./Images/visual.webp";
 export { default as vscode } from "./Images/vscode.webp";
 
 // Projects
-export { default as BiteZHero } from "./Images/BiteZHero.webp";
-export { default as BookoeHero } from "./Images/BookoeHero.webp";
-export { default as InpuctHero } from "./Images/InpuctHero.webp";
-export { default as VanguardEliteHero } from "./Images/VanguardEliteHero.webp";
+export { default as BiteZHero } from "./Images/bitezhero.webp";
+export { default as BookoeHero } from "./Images/bookoehero.webp";
+export { default as InpuctHero } from "./Images/inpucthero.webp";
+export { default as VanguardEliteHero } from "./Images/vanguardelitehero.webp";
